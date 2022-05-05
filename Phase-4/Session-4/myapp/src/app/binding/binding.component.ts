5cb486298bd389454e2cdd0c1aaed2ba2fef5660
unknown <ganitniduniyanikunj10@gmail.com>
2022-05-05 21:22:02 +0530

    Angular Notes,Create App,Component,interpolation ,PropertyBinding

---
+++ Phase-4/Session-4/myapp/src/app/binding/binding.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  name:string="Nikunj Soni";
+  url:string ="https://www.google.com";
+
+}
